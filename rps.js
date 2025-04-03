1aec923ea1ce6e00dad2c46232ae4a3298ae94a2
YohannesAH <yohannesassefa49@gmail.com>
2025-04-03 19:54:31 +0300

    editing to play for five rounds and find the winner

--- rps.js
+++ rps.js
@@ -17,6 +17,10 @@
    let humanChoice;
    let humanChoice2;
    let computerpick;
+   let humanScoreCounter = 0;
+   let computerScoreCounter = 0;
+   const maxRounds = 5;
+   let round  = 0 ;
 
    // A function creating three buttons with unique ID
     function threeButtons() {
@@ -45,31 +49,38 @@
     threeButtons();
 
     // Obtaining a human choice
-    const getHumanChoice = function () {
-    const button = document.querySelectorAll("button");
-    button.forEach((button) => {
-    button.addEventListener("click",() => {
-    humanChoice = parseInt(button.id);
+    function getHumanChoice() {
+        const button = document.querySelectorAll("button");
+        button.forEach((button) => {
+            button.addEventListener("click", () => {
+         // adding a round to play five times
+                if (round < maxRounds) {
+                    humanChoice = parseInt(button.id);
     
-    // humanChoice2 = humanChoice === 1? "Rock":
-    //                humanChoice === 2? "Paper":
-    //                "Scissors";
-    const humanChoice2 = {1: "Rock", 2: "Paper", 3: "Scissors"};
-    alert("You clicked: "+humanChoice2[humanChoice]);
+                    // humanChoice2 = humanChoice === 1? "Rock":
+                    //                humanChoice === 2? "Paper":
+                    //                "Scissors";
+                    const humanChoice2 = {1: "Rock", 2: "Paper", 3: "Scissors"};
+                    alert("You clicked: " + humanChoice2[humanChoice]);
+                    
+                    const computerRandom = generateComputerRandom();      
+                    findWinner(humanChoice,computerRandom);              
+                    round++; // Increment the round
+                }
+                if (round === maxRounds) {
+                    decideWinner(humanScoreCounter, computerScoreCounter);
+                }
+            });
+        });
+    }
     
-    const computerRandom = generateComputerRandom()
-    findWinner(humanChoice,computerRandom);
 
-    })
-    });
-   
-}
   getHumanChoice();
-
  // Get Computer value
-    const generateComputerRandom = function() {
+    function generateComputerRandom() {
         
         randomValue = (Math.floor(Math.random() * 3) + 1);
+        // The code below was original but its some sort of cluttered 
         // computerpick = randomValue === 1?"Rock":
         //                 randomValue === 2?"Paper":
         //                 "Scissors";
@@ -82,21 +93,36 @@
         }      
     // The logic to find the winner of the game
 
-    const findWinner = function(humanChoice,computerRandom) {
+    function findWinner(humanChoice,computerRandom) {
         if (
             (humanChoice === 1 && computerRandom === 3) ||
             (humanChoice === 2 && computerRandom === 1) ||
             (humanChoice === 3 && computerRandom === 2)
         ){
         alert("human Wins");  
+        humanScoreCounter ++;
         }
         else if(humanChoice === computerRandom){
-            alert("its a draw");
+        alert("its a draw");
         }
         else {
-            alert("Computer wins")
+        alert("Computer wins")
+       computerScoreCounter ++;
+        }}
+
+    function decideWinner(humanScoreCounter, computerScoreCounter) {
+        if(humanScoreCounter > computerScoreCounter){
+            alert("Humans are decissive winners")
         }
-}
+        else if (computerScoreCounter > humanScoreCounter) {
+            alert(" its a tie")
+        }
+        else {
+            alert("Computer is decissive winner");
+        }
+    }
+    
+    
 
 
 
